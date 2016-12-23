5a170632980aa2ea56e2dfbbc1fc6fa561971ba7
Krishna Prasad <krishnaprasad@Krishnas-Mac-mini.local>
2016-12-23 14:23:25 -0800

    Initial Commit

---
+++ vipinssss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  vipinssss
+//
+//  Created by Krishna Prasad on 23/12/16.
+//  Copyright © 2016 Krishna Prasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ vipinssss/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  vipinssss
+//
+//  Created by Krishna Prasad on 23/12/16.
+//  Copyright © 2016 Krishna Prasad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
